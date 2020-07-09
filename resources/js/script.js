b397414f54cb519b2e8a041153ade764817861c0
Bartosz Ornoch <bartoszornoch@pop-os.localdomain>
2020-07-09 14:11:50 +0200

    finished game physics; rewrited game elements to be OOP;

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,80 +1,10 @@
-class Game {
-
-    constructor() {
-        this.square = 50;
-        this.width = 20;
-        this.height = 10;
-        this.colors = ['white', 'red', 'green'];
-        this.gameScoreBlock = document.getElementById('score');
-        this.gameTimeBlock = document.getElementById('time');
-        this.gameScore = 0;
-        this.gameTime = 0;
-        this.gameOver = false;
-        this.timer = window.setInterval(() => { this.gameTime++, this.gameTimeBlock.innerHTML = this.gameTime }, 1000);
-    }
-    updateScore() {
-        this.gameScore += 1;
-        this.gameScoreBlock.innerHTML = this.gameScore;
-    }
-
-}
-
-class Board {
-    constructor(width, height) {
-        this.board = [];
-        for (let i = 0; i < height; i++) {
-            for (let j = 0; j < width; j++) {
-                this.board.push([j, i, 0]);
-            }
-        }
-    }
-    fillBoard(arr) {
-        for (let element of arr) {
-            let col = element[0];
-            let row = element[1];
-            this.board[col + row * 20][2] = 1;
-        }
-
-    }
-    clearBoard() {
-        for (let i = 0; i < this.board.length; i++) {
-            if (this.board[i][2] === 1) {
-                this.board[i][2] = 0;
-            }
-        }
-    }
-    placeFood() {
-        const free_space = this.board.filter(element => element[2] === 0);
-        const food_coords = free_space[Math.floor(Math.random() * free_space.length)];
-        this.board[food_coords[0] + food_coords[1] * 20][2] = 2;
-        return food_coords;
-    }
-
-    draw(surface, size = game.square) {
-        for (let element of this.board) {
-            let col, row, color;
-            [col, row, color] = [...element];
-            surface.fillStyle = game.colors[color];
-            surface.beginPath();
-            surface.rect(col * size, row * size, size, size);
-            surface.fill();
-        }
-    }
-}
-
 class Snake {
-    directions = {
-        37: [-1, 0],
-        38: [0, -1],
-        39: [1, 0],
-        40: [0, 1]
-    };
     constructor() {
         this.head = [4, 0];
         this.body = [[0, 0], [1, 0], [2, 0], [3, 0], this.head];
         this.newHead = [];
         this.horizontal = true;
-        this.previous_move = this.directions[39];
+        this.previous_move = [1, 0];
         this.isEating = false;
         this.isDead = false;
     }
@@ -88,12 +18,7 @@ class Snake {
 
     }
 
-    canMove(dx, dy, manual = true) {
-        if (manual) {
-            if (dx === this.previous_move[0] || dx === this.previous_move[0] * -1) {
-                return false;
-            }
-        }
+    canMove(dx, dy) {
         this.updateHead(dx, dy);
         if (this.body.some(x => x[0] === this.newHead[0] && x[1] === this.newHead[1])) {
             this.isDead = true;
@@ -103,7 +28,7 @@ class Snake {
         return true;
     }
 
-    move() {
+    move(dx, dy) {
         if (this.isEating) {
             this.isEating = false;
         }
@@ -123,66 +48,130 @@ class Snake {
             return true;
         };
     }
+}
+class Board {
+    constructor(width, height) {
+        this.board = [];
+        for (let i = 0; i < height; i++) {
+            for (let j = 0; j < width; j++) {
+                this.board.push([j, i, 0]);
+            }
+        }
+        this.foodCoords = [0, 0]
+    }
+    fillBoard(arr) {
+        for (let element of arr) {
+            let col = element[0];
+            let row = element[1];
+            this.board[col + row * 20][2] = 1;
+        }
 
+    }
+    clearBoard() {
+        for (let i = 0; i < this.board.length; i++) {
+            if (this.board[i][2] === 1) {
+                this.board[i][2] = 0;
+            }
+        }
+    }
+    placeFood() {
+        const free_space = this.board.filter(element => element[2] === 0);
+        this.foodCoords = free_space[Math.floor(Math.random() * free_space.length)];
+        this.board[this.foodCoords[0] + this.foodCoords[1] * 20][2] = 2;
+    }
 
-
+    draw(surface, size) {
+        for (let element of this.board) {
+            let col, row, color;
+            [col, row, color] = [...element];
+            surface.fillStyle = game.colors[color];
+            surface.beginPath();
+            surface.rect(col * size, row * size, size, size);
+            surface.fill();
+        }
+    }
 }
 
 
-var c = document.getElementById('myCanvas');
-var ctx = c.getContext('2d');
+
+class Game {
+
+    constructor() {
+        this.squareSize = 50;
+        this.width = 20;
+        this.height = 10;
+        this.colors = ['white', 'red', 'green'];
+        this.snake = new Snake();
+        this.board = new Board(this.width, this.height);
+        this.canvas = document.getElementById('myCanvas');
+        this.surface = this.canvas.getContext('2d');
+        this.gameScoreBlock = document.getElementById('score');
+        this.gameTimeBlock = document.getElementById('time');
+        this.gameScore = 0;
+        this.gameTime = 0;
+        this.gameOver = false;
+        this.gameTimer = window.setInterval(() => { this.gameTime++, this.gameTimeBlock.innerHTML = this.gameTime }, 1000);
+        this.moveTimer = window.setInterval(() => { if (this.snake.canMove(...this.snake.previous_move)) { this.moveSnake() } }, 500);
+
+    }
+
+    drawGame() {
+        this.board.clearBoard();
+        this.board.fillBoard(this.snake.body);
+        this.board.draw(this.surface, this.squareSize);
+    }
+    moveSnake() {
+        if (this.snake.checkIfEating(this.board.foodCoords)) {
+            this.board.placeFood();
+            this.updateScore();
+        };
+        this.snake.move();
+        this.drawGame();
+        window.clearInterval(this.moveTimer);
+        this.moveTimer = window.setInterval(() => { if (this.snake.canMove(...this.snake.previous_move)) { this.moveSnake() } }, 500);
+    }
+
+    handleInput(keyCode) {
+        const directions = {
+            37: [-1, 0],
+            38: [0, -1],
+            39: [1, 0],
+            40: [0, 1]
+        };
+        if (Object.keys(directions).includes(keyCode.toString())) {
+            if ((keyCode === 37 || keyCode == 39) && this.snake.horizontal) {
+                console.log('cant_move');
+                return false;
+            }
+            if ((keyCode === 38 || keyCode == 40) && !this.snake.horizontal) {
+                console.log('cant_move');
+                return false;
+            }
+            this.snake.horizontal = !this.snake.horizontal;
+            if (this.snake.canMove(...directions[keyCode])) {
+                this.moveSnake();
+            }
+        }
+        return false;
+    }
+    updateScore() {
+        this.gameScore += 1;
+        this.gameScoreBlock.innerHTML = this.gameScore;
+    }
+    stopGameTime() {
+        window.clearInteravl(this.gameTimer);
+    }
+}
+
+
 
 game = new Game();
-snake = new Snake();
-board = new Board(game.width, game.height);
-board.fillBoard(snake.body);
-let food = board.placeFood();
-board.draw(ctx);
+game.board.placeFood();
+game.drawGame();
 
-
-
-moveTimer = window.setInterval(automaticMove, 500);
-
-
-function automaticMove() {
-    if (snake.canMove(...snake.previous_move, manual = false)) {
-        if (snake.checkIfEating(food)) {
-            food = board.placeFood();
-            game.updateScore();
-        };
-        snake.move();
-    }
-    if (snake.isDead) {
-        console.log('game_over');
-    }
-    board.clearBoard();
-    board.fillBoard(snake.body);
-    board.draw(ctx);
-}
-
-
-
-document.onkeydown = function (e) {
-    console.log(snake.directions[e.keyCode]);
-    let direction = snake.directions[e.keyCode];
-    if (snake.canMove(...direction)) {
-        if (snake.checkIfEating(food)) {
-            food = board.placeFood();
-            game.updateScore();
-        };
-        snake.move();
-        window.clearInterval(moveTimer);
-        moveTimer = window.setInterval(automaticMove, 500);
-    }
-    if (snake.isDead) {
-        console.log('game_over');
-    }
-    board.clearBoard();
-    board.fillBoard(snake.body);
-    board.draw(ctx);
-}
-
-
+document.addEventListener('keydown', function (event) {
+    game.handleInput(event.keyCode);
+});
 
 
 
